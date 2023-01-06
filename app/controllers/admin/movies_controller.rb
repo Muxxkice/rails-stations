f142952aab1b75bb42a19ded65859580492efca2
rensa <rensa@rensanoMacBook-Pro.local>
2023-01-05 18:24:55 -0800

    station3 newページの作成、途中

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,5 +1,29 @@
 class Admin::MoviesController < ApplicationController
+
     def index
         @movies = Movie.all
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to("/admin/movies")
+        else
+            render 'new', status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    private
+
+        def movie_params
+            params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    get'/movies', to:'movies#index'
-#   get'/admin/movies', to:'movies#admin'
-    namespace :admin do
-        resources :movies
+namespace :admin do
+        # resources :movies
+        get'movies', to:'movies#index'
+        post'movies', to:'movies#create'
+        get'movies/new', to:'movies#new'
     end
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get'/movies', to:'movies#index'
+
+#   get'/admin/movies', to:'movies#admin'
 end
