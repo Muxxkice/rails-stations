10f17fe32c408118323d1cd976606ea5a0d81ac1
Mao Kurihara <mao.kurihara@gmail.com>
2023-01-08 20:39:39 +0900

    station4

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,29 +1,48 @@
 class Admin::MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
 
-    def index
-        @movies = Movie.all
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to("/admin/movies")
+    else
+      render "new", status: :unprocessable_entity
     end
+  end
 
-    def new
-        @movie = Movie.new
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    if @movie.update(movie_params)
+      redirect_to("/admin/movies")
+    else
+      render "edit", status: :unprocessable_entity
     end
+  end
 
-    def create
-        @movie = Movie.new(movie_params)
-        if @movie.save
-            redirect_to("/admin/movies")
-        else
-            render 'new', status: :unprocessable_entity
-        end
-    end
+  def destroy
+    @movie = Movie.find(params[:id])
+    flash[:success] = "Deleted"
+  end
 
-    def show
-        @movie = Movie.find(params[:id])
-    end
+  private
 
-    private
-
-        def movie_params
-            params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
-        end
+  def movie_params
+    params.require(:movie).permit(
+      :name,
+      :year,
+      :description,
+      :image_url,
+      :is_showing
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-namespace :admin do
-        # resources :movies
-        get'movies', to:'movies#index'
-        post'movies', to:'movies#create'
-        get'movies/new', to:'movies#new'
-    end
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    get'/movies', to:'movies#index'
+  namespace :admin do
+    resources :movies
+    # get "movies", to: "movies#index"
+    # post "movies", to: "movies#create"
 
-#   get'/admin/movies', to:'movies#admin'
+    # get "movies/new", to: "movies#new"
+    # get "movies/:id/edit", to: "movies#edit"
+    # patch "movies", to: "movies#update"
+    # put "movies/:id", to: "movies#update"
+    # delete "movies", to: "movies#destroy"
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/movies", to: "movies#index"
 end
